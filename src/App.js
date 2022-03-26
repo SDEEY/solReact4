5bccb80f2e07482eb1150f50e0e0f463d42469be
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-03-26 19:41:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.1
-const image = 'https://pbs.twimg.com/profile_images/1492864592309010434/VWuIe-4a_400x400.jpg'
-const Title = 'NEON METAVERSE'
+const solAmount = 0.05
+const image = 'https://pbs.twimg.com/profile_images/1505992981370839041/lzSECZec_400x400.jpg'
+const Title = 'Derug Pugs NFT'
 
 document.title = Title
 
