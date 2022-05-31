0653ea6bca1c3f8480581de4af7c3824f029e197
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-05-31 23:02:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.5
+const solAmount = 0.3
 const image = 'https://howrare.is/drop_logos/5084_DCKnWFVY.jpg' 
 const Title = 'Dragon Elements'
 const supply = 1110
